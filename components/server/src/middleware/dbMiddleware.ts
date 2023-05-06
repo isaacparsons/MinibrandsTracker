9db91a49e291ae9f4b8f72abb6b5bd75fd5b35e1
isaacparsons <isaac.2962@gmail.com>
2023-05-06 09:15:05 -0600

    try to fix too many db connections

--- components/server/src/middleware/dbMiddleware.ts
+++ components/server/src/middleware/dbMiddleware.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 import { Express, Request, Response, NextFunction } from "express";
 
+const prisma = new PrismaClient();
+
 export function dbMiddleware(req: Request, res: Response, next: NextFunction): void {
-  req.prisma = new PrismaClient();
+  req.prisma = prisma;
   next();
 }
